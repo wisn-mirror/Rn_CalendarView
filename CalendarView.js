ab73aa0356ff858d52925f7b13f9369b4bac30f1
Wisn <wuyishun_kmk@outlook.com>
2017-09-03 21:10:30 +0800

    currentDayStyle

--- CalendarView.js
+++ CalendarView.js
@@ -4,6 +4,7 @@ import {
     Text,
     TouchableOpacity,
     StyleSheet,
+    ScrollView,
 } from 'react-native';
 
 import Dimensions from 'Dimensions';
@@ -40,7 +41,9 @@ export default class CalendarView extends Component {
         //是否显示头部
         isShowHeader: PropTypes.bool,
         //指定默认选中
-        selectDay:PropTypes.number,
+        selectDay: PropTypes.number,
+        //是否左右滑动
+        isScroll: PropTypes.bool,
     }
 
     constructor(props) {
@@ -50,16 +53,24 @@ export default class CalendarView extends Component {
             month: (this.props.month && this.props.month <= 12 && this.props.month > 0) ? this.props.month : this.getNowMonth(),
             isShow: true,
             isShowStr: '收起',
-            select: this.props.selectDay?this.props.selectDay:-1,
+            select: this.props.selectDay ? this.props.selectDay : -1,
             head: this.props.head ? this.props.head : (this.props.isEN ? weekEN : weekCN),
             isShowHeader: this.props.isShowHeader ? this.props.isShowHeader : false,
+            currentScrollIndex: 1,
+            isScroll: this.props.isScroll ? this.props.isScroll : true,
         };
     }
 
     componentDidMount() {
-       /* this.setState({
-            select: this.mGetTodyDate(),
-        });*/
+        if (this.state.isScroll) {
+            this.refs.scrollView.scrollResponderScrollTo({x: (1 * SCREEN_WIDTH), y: 0, animated: false});
+        }
+    }
+
+    componentDidUpdate() {
+        if (this.state.isScroll) {
+            this.refs.scrollView.scrollResponderScrollTo({x: (1 * SCREEN_WIDTH), y: 0, animated: false});
+        }
     }
 
     componentWillReceiveProps(nextProps) {
@@ -88,8 +99,14 @@ export default class CalendarView extends Component {
             this.setState({
                 select: nextProps.selectDay ? nextProps.selectDay : false,
             })
+        } //更新选中天
+        if (this.state.isScroll !== nextProps.isScroll) {
+            this.setState({
+                isScroll: nextProps.isScroll ? nextProps.isScroll : false,
+            })
         }
     }
+
     //底部是否隐藏日历
     _PressIsShow() {
         this.setState({
@@ -99,20 +116,52 @@ export default class CalendarView extends Component {
     }
 
     render() {
+        let content = this.state.isScroll ? <ScrollView
+            ref='scrollView'
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            pagingEnabled={true}
+            scrollEnabled={true}
+            removeClippedSubviews={true}
+            //当一帧滚动结束
+            onMomentumScrollEnd={(e) => {
+                var offSetX = e.nativeEvent.contentOffset.x;
+                var currentPageIndex = Math.floor(offSetX / SCREEN_WIDTH);
+                if (this.state.currentScrollIndex !== currentPageIndex && currentPageIndex !== 1) {
+                    var month = currentPageIndex === 2 ? this.state.month + 1 : this.state.month - 1;
+                    var year = this.state.year;
+                    if (month > 12) {
+                        year = this.state.year + 1;
+                        month = 1;
+                    } else if (month < 1) {
+                        year = this.state.year - 1;
+                        month = 12;
+                    }
+                    this.setState({
+                        year: parseInt(year),
+                        month: parseInt(month),
+                    });
+                    this.refs.scrollView.scrollResponderScrollTo({x: (1 * SCREEN_WIDTH), y: 0, animated: false});
+                }
+            }}
+        >
+            {this.getScrollDataView()}
+        </ScrollView> : this.getDataListView(this.state.year, this.state.month);
         return (
             <View
                 style={[styles.container, this.props.calendarStyle]}>
                 {this.getHeader()}
-                <View style={[styles.outLineViewStyle,this.props.titleHeaderRowStyle]}>
+                <View style={[styles.outLineViewStyle, this.props.titleHeaderRowStyle]}>
                     {this.getTitleView()}
                 </View>
-                {this.getDataListView()}
+                {content}
                 <TouchableOpacity onPress={() => this._PressIsShow()}>
                     <Text style={styles.bottomStyle}>{this.state.isShowStr}</Text>
                 </TouchableOpacity>
             </View>
         );
     }
+
 
     getHeader() {
         if (this.state.isShowHeader) {
@@ -142,10 +191,19 @@ export default class CalendarView extends Component {
         });
     }
 
-    getDataListView() {
-        var MonthDaySum = this.mGetDate();
-        var weekStart = this.mGetDataWeek();
-        var today = this.mGetTodyDate();
+    getScrollDataView() {
+        var views = [];
+        views.push(<View key={1}>{this.getDataListView(this.state.year, this.state.month - 1)}</View>)
+        views.push(<View key={2}>{this.getDataListView(this.state.year, this.state.month)}</View>)
+        views.push(<View key={3}>{this.getDataListView(this.state.year, this.state.month + 1)}</View>)
+        return views;
+    }
+
+
+    getDataListView(year, month) {
+        var MonthDaySum = this.mGetDate(year, month);
+        var weekStart = this.mGetDataWeek(year, month);
+        var today = this.mGetTodyDate(year, month);
         var numItemByWeek = (MonthDaySum + weekStart) / 7;//4
         var OutViews = [];
         var ItemIndex = 0;
@@ -165,7 +223,10 @@ export default class CalendarView extends Component {
                         var weekendStyle = {};
                         if (date === this.state.select) {
                             //选中的样式
-                            selectStyle =this.props.selectDayStyle?this.props.selectDayStyle: {backgroundColor: "#ff9821", color: 'white'};
+                            selectStyle = this.props.selectDayStyle ? this.props.selectDayStyle : {
+                                backgroundColor: "#ff9821",
+                                color: 'white'
+                            };
                         } else {
                             selectStyle = null;
                         }
@@ -176,12 +237,13 @@ export default class CalendarView extends Component {
                             weekendStyle = null;
                         }
                         if (ItemIndex === (today + weekStart)) {
+                            var currentDayStyle = this.props.currentDayStyle? this.props.currentDayStyle: {backgroundColor: "#72ff17"};
                             //今天
                             RowViews.push(
                                 <TouchableOpacity key={ItemIndex}
                                                   onPress={this._pressDay.bind(this, this.state.year, date)}>
                                     <Text
-                                        style={[styles.monthDayStyle, {backgroundColor: "#72ff17"}, selectStyle, weekendStyle,this.props.currentDayStyle]}
+                                        style={[styles.monthDayStyle, weekendStyle,currentDayStyle, selectStyle]}
                                     >{date}</Text>
                                 </TouchableOpacity>)
                         } else {
@@ -189,7 +251,8 @@ export default class CalendarView extends Component {
                             RowViews.push(
                                 <TouchableOpacity key={ItemIndex}
                                                   onPress={this._pressDay.bind(this, this.state.year, date)}>
-                                    <Text style={[styles.monthDayStyle, this.props.dayStyle,weekendStyle, selectStyle]}>{date}</Text>
+                                    <Text
+                                        style={[styles.monthDayStyle, this.props.dayStyle, weekendStyle, selectStyle]}>{date}</Text>
                                 </TouchableOpacity>)
 
                         }
@@ -201,7 +264,7 @@ export default class CalendarView extends Component {
                     date++;
                 }
             }
-            OutViews.push(<View key={i} style={[styles.outLineViewStyle,this.props.dayRowStyle]}>
+            OutViews.push(<View key={i} style={[styles.outLineViewStyle, this.props.dayRowStyle]}>
                 {RowViews}
             </View>)
         }
@@ -222,7 +285,7 @@ export default class CalendarView extends Component {
         var arr = this.state.head;
         for (var i = 0; i < arr.length; i++) {
             views.push(<Text key={i}
-                             style={[styles.titleStyle,this.props.titleHeaderStyle]}>{arr[i]}</Text>)
+                             style={[styles.titleStyle, this.props.titleHeaderStyle]}>{arr[i]}</Text>)
         }
         return views;
     }
@@ -231,10 +294,10 @@ export default class CalendarView extends Component {
      *  当月多少天
      * @returns {number}
      */
-    mGetDate() {
-        if (this.state.year !== null && this.state.month !== null) {
+    mGetDate(year, month) {
+        if (year !== null && month !== null) {
             //指定日期
-            var d = new Date(this.state.year, this.state.month, 0);
+            var d = new Date(year, month, 0);
             return d.getDate();
         } else {
             var d = new Date();
@@ -249,13 +312,29 @@ export default class CalendarView extends Component {
      * 获取当前日
      * @returns {number}
      */
-    mGetTodyDate() {
+    mGetTodyDate(year, month) {
         var d = new Date();
-        // console.log("TAG"+d.getFullYear() +"  " +this.state.year+" "+(d.getMonth() + 1) +"   "+ this.state.month)
-        if (d.getFullYear() === this.state.year && (d.getMonth() + 1) === this.state.month) {
+        if (d.getFullYear() === year && (d.getMonth() + 1) === month) {
             return d.getDate();
         } else {
             return -1;
+        }
+    }
+
+    /**
+     * 当月第一天星期几
+     * @returns {number}
+     */
+    mGetDataWeek(year, month) {
+        if (year !== null && month !== null) {
+            //指定日期
+            var d = new Date(year, month, 0);
+            d.setDate(1);
+            return d.getDay();
+        } else {
+            var d = new Date();
+            d.setDate(1);
+            return d.getDay();
         }
     }
 
@@ -277,22 +356,6 @@ export default class CalendarView extends Component {
         return d.getMonth();
     }
 
-    /**
-     * 当月第一天星期几
-     * @returns {number}
-     */
-    mGetDataWeek() {
-        if (this.state.year !== null && this.state.month !== null) {
-            //指定日期
-            var d = new Date(this.state.year, this.state.month, 0);
-            d.setDate(1);
-            return d.getDay();
-        } else {
-            var d = new Date();
-            d.setDate(1);
-            return d.getDay();
-        }
-    }
 
 }
 
--- index.main.js
+++ index.main.js
@@ -6,21 +6,16 @@
 
 import React, {Component} from 'react';
 import {
-    AppRegistry,
-    StyleSheet,
-    Text,
     View,
     Alert,
 } from 'react-native';
 import CalendarView from "./CalendarView";
-import ScrollCalendarView from "./ScrollCalendarView";
-import C from "./C";
 
 export default class MainView extends Component {
     render() {
         return (
             <View style={{marginTop:20}}>
-            <ScrollCalendarView
+            <CalendarView
                 year={2017}
                 month={9}
                 isEN={true}
@@ -37,29 +32,3 @@ export default class MainView extends Component {
         );
     }
 }
-/*
-calendarStyle: View.propTypes.style,
-        //星期的一行的样式
-        titleHeaderRowStyle: View.propTypes.style,
-        //星期字体的样式
-        titleHeaderStyle: View.propTypes.style,
-        //正常天数的样式
-        dayStyle: View.propTypes.style,
-        //正常天数的一行的样式
-        dayRowStyle: View.propTypes.style,
-        //选中的样式
-        selectDayStyle: View.propTypes.style,
-        //当天选中的样式
-
-
-* */
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-
-});
